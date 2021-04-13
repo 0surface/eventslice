6471115c512bffc1b54f219c7ff0527716e42d63
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-04-13 21:52:53 +0100

    button

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableWithoutFeedback,
-  TouchableHighlight,
-  TouchableOpacity,
-  Image,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Text is pressed");
-  const handleTouWFPress = () => console.log("Image pressed");
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello world of react native
-      </Text>
-      {/* <Image source={require("./assets/icon.png")} /> */}
-      <TouchableHighlight onPress={handleTouWFPress}>
-        {/* <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        /> */}
-        <View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}></View>
-      </TouchableHighlight>
+      <Button color="orange" title="Click Me" onPress={() => console.log("button pressed")} />
     </SafeAreaView>
   );
 }
