bdc9fe6764a43f09017da83af33032e960daae8a
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-04-13 22:25:49 +0100

    dimensions of  device under test

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Button, StatusBar, Platform, View } from "react-native";
+import { Dimensions, StyleSheet, SafeAreaView, Button, StatusBar, Platform, View } from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
   return (
     <SafeAreaView style={styles.container}>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "50%",
-          height: 70,
+          width: "50%", //this is % value
+          height: 70, // this is a DIP
         }}
         //Density-Independent Pixles : Physical pixles = DIPS x scale factor
       ></View>
