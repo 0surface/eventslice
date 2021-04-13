7f3c4f760c622821e616d0e9322a4dfd7bac2576
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-04-13 22:03:02 +0100

    Button with Alert

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, SafeAreaView, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Alert, Button } from "react-native";
 
 export default function App() {
+  const handleNoAlertResponse = () => console.log("No");
+  const handleYesAlertResponse = () => console.log("Yes");
   return (
     <SafeAreaView style={styles.container}>
-      <Button color="orange" title="Click Me" onPress={() => console.log("button pressed")} />
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My Title", "My message", [
+            { text: "Yes", onPress: handleYesAlertResponse },
+            { text: "No", onPress: handleNoAlertResponse },
+          ])
+        }
+      />
     </SafeAreaView>
   );
 }
